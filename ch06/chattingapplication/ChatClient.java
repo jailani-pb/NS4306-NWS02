d7462410141eba49086ce1df13539edfe1ad9a5a
Jailani Rahman <jailani.har@googlemail.com>
2017-03-15 17:52:34 +0800

    Implemented chatting application

---
+++ ch06/chattingapplication/ChatClient.java
@@ -0,0 +1,55 @@
+package ch06.chattingapplication;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ChatClient {
+
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost", 9101);
+			
+			DataInputStream fromServer = new DataInputStream(
+					new BufferedInputStream(socket.getInputStream()));
+			DataOutputStream toServer = new DataOutputStream(
+					new BufferedOutputStream(socket.getOutputStream()));
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						while(true) {
+							if(fromServer.available() > 0) {
+								String receiveMsg = fromServer.readUTF();
+								System.out.println(">>> " + receiveMsg);
+							}
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}).start();
+			
+			while(true) {
+				String sendMsg = scanner.nextLine().trim();
+				toServer.writeUTF(sendMsg);
+				toServer.flush();
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
