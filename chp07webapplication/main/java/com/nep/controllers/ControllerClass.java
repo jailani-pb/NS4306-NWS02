dd1a0013addf6e70341f919afa769e07e63f6c0a
Jailani Rahman <jailani.har@googlemail.com>
2017-04-18 16:05:46 +0800

    Web application that connects to Database

--- chp07webapplication/main/java/com/nep/controllers/ControllerClass.java
+++ chp07webapplication/main/java/com/nep/controllers/ControllerClass.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nep.data.ProductRepository;
@@ -19,19 +20,173 @@ public class ControllerClass {
 
 	@RequestMapping(value="/")
 	public String index(ModelMap modelMap) {
-		List<Product> products = productRepository.getAllProducts();
+		List<Product> products = (List<Product>) productRepository.findAll();
 		modelMap.put("products", products);
 		return "index";
 	}
 	
-	@RequestMapping(value="/product/{name}")
-	public String product(@PathVariable String name, ModelMap modelMap) {
-		Product product = productRepository.findProductByName(name);
-		modelMap.put("product", product);
+	@RequestMapping(value="/product/{id}")
+	public String product(@PathVariable String id, ModelMap modelMap) {
+		try {
+			long idInLong = Long.parseLong(id);
+			if(productRepository.exists(idInLong)) {
+				Product product = productRepository.findOne(idInLong);
+				modelMap.put("product", product);
+			}
+		} catch (NumberFormatException e) {
+			
+		}
 		return "product";
 	}
 	
+	@RequestMapping(value="/search")
+	public String search(@RequestParam(required=false) String productName, ModelMap modelMap) {
+		List<Product> products;
+		if(productName == null) {
+			// Show all products
+			products = (List<Product>) productRepository.findAll();
+			modelMap.put("products", products);
+		} else {
+			// Show the search products
+			products = productRepository.findByNameIgnoreCase(productName);
+			modelMap.put("products", products);
+		}
+		return "search";
+	}
+	
+	@RequestMapping(value="/addproduct")
+	public String addProduct(
+			@RequestParam(required=false) String name,
+			@RequestParam(required=false) String price,
+			@RequestParam(required=false) String file,
+			@RequestParam(required=false) String instock,
+			ModelMap modelMap
+			) {
+		boolean addSuccessful = false;
+		boolean addFailed = false;
+		if(name==null && price==null 
+				&& file==null && instock==null) {
+			addSuccessful = false;
+			addFailed = false;
+		} else {
+			if(name==null || price==null 
+					|| file==null || instock==null) {
+				addSuccessful = false;
+				addFailed = true;
+			} else {
+				try {
+					double priceInDouble = Double.parseDouble(price);
+					boolean instockInBoolean = Boolean.parseBoolean(instock);
+					Product product = new Product(name, priceInDouble, 
+							file, instockInBoolean);
+					productRepository.save(product);
+					addSuccessful = true;
+					addFailed = false;
+				} catch (NumberFormatException e) {
+					addSuccessful = false;
+					addFailed = true;
+				}
+			}
+		}
+		modelMap.put("successful", addSuccessful);
+		modelMap.put("failed", addFailed);
+		return "addproduct";
+	}
+	
+	@RequestMapping(value="removeproduct")
+	public String removeProduct(
+			@RequestParam(required=false) String id
+			) {
+		if(id==null) {
+			
+		} else {
+			try {
+				long idInLong = Long.parseLong(id);
+				Product product = productRepository.findOne(idInLong);
+				if(product==null) {
+					
+				} else {
+					productRepository.delete(product);
+				}
+			} catch (NumberFormatException e) {
+				
+			}
+		}
+		return "removeproduct";
+	}
+	
+	@RequestMapping(value="/findproduct")
+	public String findProduct(
+			@RequestParam(required=false) String id
+			) {
+		if(id == null) {
+			return "findproduct";
+		} else {
+			try {
+				long idInLong = Long.parseLong(id);
+				if(productRepository.exists(idInLong)) {
+					return "redirect:/updateproduct/" + idInLong;
+				} else {
+					return "findproduct";
+				}
+			} catch (NumberFormatException e) {
+				return "findproduct";
+			}
+		}
+	}
+	
+	@RequestMapping(value="/updateproduct/{id}")
+	public String updateProduct(
+			@PathVariable String id,
+			@RequestParam(required=false) String name,
+			@RequestParam(required=false) String price,
+			@RequestParam(required=false) String file,
+			@RequestParam(required=false) String instock,
+			ModelMap modelMap
+			) {
+		try {
+			long idInLong = Long.parseLong(id);
+			Product product = productRepository.findOne(idInLong);
+			if(product == null) {
+				return "redirect:/findproduct";
+			} else {
+				if(name==null && price==null
+						&& file==null && instock==null) {
+					
+				} else {
+					if(name==null || price==null
+						|| file==null || instock==null) {
+						
+					} else {
+						try {
+							double priceInDouble = Double.parseDouble(price);
+							boolean instockInBoolean = Boolean.parseBoolean(instock);
+							
+							product.setName(name);
+							product.setPrice(priceInDouble);
+							product.setPicFile(file);
+							product.setInStock(instockInBoolean);
+							productRepository.save(product);
+							modelMap.put("product", product);
+							return "updateproduct";
+						} catch (NumberFormatException e) {
+							
+						}
+					}
+				}
+				modelMap.put("product", product);
+				return "updateproduct";
+			}
+		} catch (NumberFormatException e) {
+			return "redirect:/findproduct";
+		}
+	}
 }
+
+
+
+
+
 
 
 
--- chp07webapplication/main/java/com/nep/data/ProductRepository.java
+++ chp07webapplication/main/java/com/nep/data/ProductRepository.java
@@ -1,38 +1,60 @@
 package com.nep.data;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Component;
+import org.springframework.data.repository.CrudRepository;
 
 import com.nep.models.Product;
 
-@Component
-public class ProductRepository {
+//@Component
+@Transactional
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
-	private static final List<Product> ALL_PRODUCTS = Arrays.asList(
-			new Product("Apple iPhone 6s", 1210.55, "iphone6s.png", true),
-			new Product("Apple iPad Pro", 1310.55, "ipadpro.png", true),
-			new Product("Samsung Galaxy S7 Edge", 801.32, "samsungs7edge.png", true),
-			new Product("Samsung Galaxy Note 7", 1035.99, "samsungnote7.png", false),
-			new Product("HTC One X9", 500.99, "htconex9.png", false),
-			new Product("Oppo F1s", 309.80, "oppof1s.png", true)
-			);
+	List<Product> findByNameIgnoreCase(String name);
 	
-	public List<Product> getAllProducts() {
-		return ALL_PRODUCTS;
-	}
-	
-	public Product findProductByName(String name) {
-		for(Product product : ALL_PRODUCTS) {
-			if(product.getName().equals(name)) {
-				return product;
-			}
-		}
-		return null;
-	}
+//	private static final List<Product> ALL_PRODUCTS = Arrays.asList(
+//			new Product("Apple iPhone 6s", 1210.55, "iphone6s.png", true),
+//			new Product("Apple iPad Pro", 1310.55, "ipadpro.png", true),
+//			new Product("Samsung Galaxy S7 Edge", 801.32, "samsungs7edge.png", true),
+//			new Product("Samsung Galaxy Note 7", 1035.99, "samsungnote7.png", false),
+//			new Product("HTC One X9", 500.99, "htconex9.png", false),
+//			new Product("Oppo F1s", 309.80, "oppof1s.png", true)
+//			);
+//	
+//	public List<Product> getAllProducts() {
+//		return ALL_PRODUCTS;
+//	}
+//	
+//	public Product findProductByName(String name) {
+//		for(Product product : ALL_PRODUCTS) {
+//			if(product.getName().equals(name)) {
+//				return product;
+//			}
+//		}
+//		return null;
+//	}
+//
+//	public List<Product> getProductContainName(String name) {
+//		List<Product> products = new ArrayList<Product>();
+//		for(Product product : ALL_PRODUCTS) {
+//			if(product.getName().toLowerCase().contains(name.toLowerCase())) {
+//				products.add(product);
+//			}
+//		}
+//		return products;
+//	}
 	
 }
+
+
+
+
+
 
 
 
